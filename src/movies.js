69cd50995fdb49786a317ab76631f64d780d3ac6
Miguel Curbelo <mcurbeloj@gmail.com>
2022-09-28 16:49:44 +0200

    adds iteration 3 4 and 5, with 3 and 5 missing the last test verification

--- src/movies.js
+++ src/movies.js
@@ -11,13 +11,34 @@ function howManyMovies(moviesArray) {
 }
 
 // Iteration 3: All scores average - Get the average of all scores with 2 decimals
-function scoresAverage(moviesArray) {}
+function scoresAverage(moviesArray) {
+    if (!moviesArray.length){return 0};
+    const scoredMovies = moviesArray.filter(film => film.score !== undefined);
+    const totalScore = scoredMovies.reduce((acc,curVal) => acc + curVal.score, 0);
+    const avgScore = totalScore/scoredMovies.length;
+    return parseFloat(avgScore.toFixed(2));
+}
 
 // Iteration 4: Drama movies - Get the average of Drama Movies
-function dramaMoviesScore(moviesArray) {}
+function dramaMoviesScore(moviesArray) {
+    const dramaMovies = moviesArray.filter(film => film.genre.includes ('Drama'));
+    if (!dramaMovies.length){return 0};
+    const dramaTotalScore = dramaMovies.reduce((accumulator,currentScore) => accumulator + currentScore.score,0);
+    const dramaAvgScore =dramaTotalScore/dramaMovies.length;
+    return parseFloat(dramaAvgScore.toFixed(2))
+}
 
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
-function orderByYear(moviesArray) {}
+function orderByYear(moviesArray) {
+    const mappedArray = moviesArray.map(x => x);
+    //mappedArray.sort(function (a,b){
+//        if (a.year<b.year) return -1;
+ //       if (a.year>b.year) return 1;
+  //      if (a.year===b.year)
+  //          return (sort ((a,b) => a.title.toLowerCase() - b.title.toLowerCase()));
+  //  })
+   return mappedArray.sort((a,b) => a.year - b.year);;
+}
 
 // Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
 function orderAlphabetically(moviesArray) {}
